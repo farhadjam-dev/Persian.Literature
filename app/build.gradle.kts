604c8cc767c50c3b360986dd07a07bdac886a476
farhadjam-dev <jamshidpour.farhad@gmail.com>
2025-03-11 13:22:47 +0330

    تغییرات سرچ و منو کشویی و علاقع مندی ها تکمیل شد نغیییرات تولبار صفحه حافظ اضافه کردن پلیر تکمیل صفحه حافظ (زندگی نامه و غزلیات و زباعیات و قطعات و) قصایدو اشعار منسب و مثنوی و ساقی نامه بخش سعدی صفحه اول  ( غزلیات و رباعیات و ملحقات و ترجیع بند شماره ابیات بوستان سعدی گلستان مواعظ بوستان کامل شد مواعظ کاامل شد گلستان کامل شد رسایل و مجالس کامل صفحه ی در باره ما

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment:2.8.7")
     implementation ("androidx.navigation:navigation-ui:2.8.7")
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
+
+
 }
